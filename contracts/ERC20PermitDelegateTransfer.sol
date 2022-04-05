8201104f072a62acd2fa68a46dbd34cdb4a69610
Adam Levi <leviadam@gmail.com>
2022-04-05 14:58:47 +0300

    Add __gap

--- contracts/ERC20PermitDelegateTransfer.sol
+++ contracts/ERC20PermitDelegateTransfer.sol
@@ -103,4 +103,11 @@ contract ERC20PermitDelegateTransfer is ERC20Upgradeable {
             )
         );
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[50] private __gap;
 }
